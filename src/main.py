2a23af9ba23132d1d23db8e6900d26452552fded
shahriar <shahriaarrr@gmail.com>
2022-03-27 17:43:07 +0430

    change class name and setWindowTitle()

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-class GmailSender(QMainWindow):
+class SHcode(QMainWindow):
     def __init__(self):
-        super(GmailSender, self).__init__()
+        super(SHcode, self).__init__()
         uic.loadUi('SHcode.ui', self)
         self.show()
 
+        self.setWindowTitle("SHcode")
+
 def main():
     app = QApplication([])
-    window = GmailSender()
+    window = SHcode()
     app.exec_()
 
 if __name__ == '__main__':
